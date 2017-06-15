9e389ca8289ff5e286b91be077da24321450e7aa
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-06-15 11:16:37 +0300

    Create a fullname prototype function created a fullname property from the protoype object that takes the firstname and last name and joins them

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-// Business logic 
-var contact=function(firstname,lastname){
+// Business logic
+var Contact=function(firstname,lastname){
   this.firstname=firstname;
   this.lastname=lastname;
 }
 
+Contact.prototype.fullName = function () {
+  return this.firstname + " " + this.lastname;
+
+
+};
 
 // User interface logic
