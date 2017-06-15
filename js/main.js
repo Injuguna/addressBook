f156c749fbcb7dddda5f2a34d9092a34c91f193d
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-06-15 10:11:31 +0300

    Create contact constructor
    created a contact constructor that has the firstname and lastname  of  a
    contact

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
-$.ajax({
-method: 'GET',
-url: 'http://api.openweathermap.org/data/2.5/weather?q=San%20Francisco&mode=json&units=imperial&APPID=3943eeb5a4c109f4da80636740ea193',
-statusCode :{
-  401:function () {
-    alert("You have no access");
-  }
-},
-success:function(data){
-console.log(data);
-},
-failure:function(error){
-	console.log(Error)
+// Business logic 
+var contact=function(firstname,lastname){
+  this.firstname=firstname;
+  this.lastname=lastname;
 }
-});
+
+
+// User interface logic
