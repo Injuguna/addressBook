f8cd3be572311858b70bd1f54a10698b4fd156b3
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-06-15 11:27:27 +0300

    Create new address created the function that will create new input forms when the another address button in html is clicked

--- js/main.js
+++ js/main.js
@@ -6,8 +6,34 @@ var Contact=function(firstname,lastname){
 
 Contact.prototype.fullName = function () {
   return this.firstname + " " + this.lastname;
-
-
 };
 
 // User interface logic
+$(document).ready(function(){
+
+  // handling the add addresses click function
+  $("#addAddress").click(function(){
+    $("#address").append(
+      `
+      <h2>Another address</h2>
+      <div class="form-group">
+          <label for="street">Street</label>
+          <input type="text" placeholder="street" class="form-control street" required="">
+      </div>
+      <!-- city -->
+      <div class="form-group">
+          <label for="city">City</label>
+          <input type="text" placeholder="city" class="form-control city" required="">
+      </div>
+      <!-- county -->
+      <div class="form-group">
+          <label for="county">County</label>
+          <input type="text" placeholder="county" class="form-control county" required="">
+      </div>
+
+      `
+    );
+    // end of the append method
+  });
+
+});
